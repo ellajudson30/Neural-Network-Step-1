636f80404302f6f5bfe7c58805afb640391b2492
Ella Judson <ellajudson30@gmail.com>
2026-07-01 20:38:48 -0700

    Separated workflow into 3 files

--- Test Data/generate_data.py
+++ Test Data/generate_data.py
@@ -2,12 +2,9 @@ import numpy as np
 import math
 import random 
 from scipy.integrate import RK45
-import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, Subset, DataLoader
-import torch.optim as optim
 import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold, train_test_split
+import csv
+from rk45utils import *
 
 # Exponential Decay
 lmda = 2  # increase to get more points
@@ -33,47 +30,6 @@ o_t_span = [0, 4*math.pi]
 o_y0 = [-2, -1, -0.5, 0.5, 1, 2]
 def oscil(t,y):
     return y*math.cos(t)
-
-# Function to run RK45
-def run_RK45(fcn, t0, y0, tf, tol):
-
-    solver = RK45(fcn,t0, y0, tf, rtol=tol, atol=tol)
-    
-    # Storage arrays
-    times = []
-    solution = []
-    time_steps = []
-    errors = []
-    stage_history = []
-
-    while solver.t < tf:
-        solution.append(solver.y[0]) # only works if one IC passed
-        times.append(float(solver.t))
-        
-        t_old = solver.t
-        
-        solver.step()
-        h = solver.t - t_old
-        time_steps.append(float(h))
-        stage_history.append(solver.K.copy())
-
-        scale = solver.atol + solver.rtol * np.maximum(np.abs(solver.y_old), 
-                                                    np.abs(solver.y))
-        
-        err = solver._estimate_error_norm(solver.K, h, scale)
-
-        errors.append(float(err))
-
-    return times, solution, time_steps, errors, stage_history
-
-# Approximate derviatives
-def approx_first_deriv2(sol, times):
-    y_prime = np.zeros(len(times) -2 )
-
-    y_prime = (np.array(sol[2:]) - np.array(sol[:-2]))/(np.array(times[2:])
-                                                     - np.array(times[:-2]))
-    return y_prime
-def approx_second_deriv2(sol, times):
     y_2prime = np.zeros(len(times) -2 )
 
     times = np.array(times)
@@ -121,8 +77,10 @@ def build_features(indices, solution, time_steps, stage_history):
         next_step_ratio.append(r_next)
 
     return data_list, next_step_ratio
-def build_features_solhist(indices, solution, time_steps, num_hist):
-
+def build_features_solhist(indices, solution, time_steps, num_hist, tol):
+    """ indices - of chosen data points to be added to dataset
+    num_hist - number of previous solution points (adds numhist+1 items to feature)
+    """
     data_list = []
     next_step_ratio = []
     for i in indices:
@@ -140,6 +98,8 @@ def build_features_solhist(indices, solution, time_steps, num_hist):
             for j in range(num_hist, -1, -1):
                 r = time_steps[i-j]/time_steps[i-j-1]
                 feature.append(r)
+
+            feature.append(np.log(tol))
 
             data_list.append(feature)
 
@@ -222,7 +182,7 @@ def generate_data(fcn, t0, y0, tf, tol):
 
             # Create features/ratios and add to Dataset
             # add log of tol 
-            data, ratios = build_features_solhist(indices, sol, ts, 4)
+            data, ratios = build_features_solhist(indices, sol, ts, 4, j)
             # data, ratios = build_features(indices, sol, ts, sh)
 
             Data_set.extend(data)
@@ -266,181 +226,16 @@ print("Total examples: ", len(Data_set))
 
 print(f"Size of Data Matrix: {len(Data_set)}x{len(Data_set[0])}" )
 
-# Create Pytorch Dataset 
-class myDataset(Dataset):
-    def __init__(self, features, labels, transform=None):
-        self.features = torch.tensor(features, dtype=torch.float32)
-        self.labels = torch.tensor(labels, dtype=torch.long)
-        self.transform = transform
+# Store Dataset as a csv file
+# with open("features.csv", mode="w", newline="", encoding="utf-8") as file:
+#     writer = csv.writer(file)
+#     writer.writerows(Data_set)
+
+# with open("ratios.csv", mode="w", newline="", encoding="utf-8") as file:
+#     writer = csv.writer(file)
+#     writer.writerows([[r] for r in Ratios])
+
     
-    def __len__(self):
-        return len(self.features)
-    
-    def __getitem__(self, idx):
-        x = self.features[idx]
-        y = self.labels[idx]
-        return x,y
-
-class NN1(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(10,32),
-            nn.Tanh(), 
-            nn.Linear(32,32),
-            nn.Tanh(),
-            nn.Linear(32,1)
-        ) 
-    
-    def forward(self, x):
-        output = self.net(x)
-        return output
-
-class NN2(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(10,24),
-            nn.Tanh(), 
-            nn.Linear(24,24),
-            nn.Tanh(),
-            nn.Linear(24,1)
-        ) 
-    
-    def forward(self, x):
-        output = self.net(x)
-        return output
-
-class NN3(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(10,16),
-            nn.Tanh(), 
-            nn.Linear(16,16),
-            nn.Tanh(),
-            nn.Linear(16,16),
-            nn.Tanh(),
-            nn.Linear(16,1)
-        ) 
-    
-    def forward(self, x):
-        output = self.net(x)
-        return output
-Dataset = myDataset(Data_set, Ratios)
-
-# Split into training and test sets 
-Training_data, Testing_data = train_test_split(Dataset, test_size=0.2, random_state=42)
-
-# print(len(Training_data))
-# print(len(Testing_data))
-
-kfold = KFold(n_splits=4, shuffle=True, random_state=42)
-
-num_epochs = 10
-CV_results = []
-
-# Do CV on Training Data
-for fold, (train_index, val_index) in enumerate(kfold.split(Training_data)):
-    
-    TrainData = Subset(Dataset, train_index)
-    ValData = Subset(Dataset, val_index)
-
-    batch = 16
-    Train_loader = DataLoader(TrainData, batch_size=batch)
-    Val_loader = DataLoader(ValData, batch_size=batch)
-
-    # Define model, loss function and optimizer
-    # model = NN1()
-    # model = NN2()
-    model = NN3()
-    loss_fcn = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-    # Training
-    for epoch in range(num_epochs):
-        model.train()
-
-        for batch, (X,y) in enumerate(Train_loader):
-            y = y.float().unsqueeze(1)
-            optimizer.zero_grad()
-            output = model(X)
-            loss = loss_fcn(output, y)
-            loss.backward()
-            optimizer.step()
-    
-    # Validation
-    model.eval()
-    val_loss = 0.0
-    
-    with torch.no_grad():
-        for batch, (X,y) in enumerate(Val_loader):
-            y = y.float().unsqueeze(1)
-            test_output = model(X)
-            loss = loss_fcn(test_output, y)
-
-            val_loss += loss.item()
-
-    print(f"Fold {fold+1} MSE: ", val_loss)
-    CV_results.append(val_loss)
-
-print("Average MSE: ", sum(CV_results)/4)
-
-# Choose final set-up and retrain on whole training data
-batch = 32
-Train_loader = DataLoader(Training_data, batch_size=batch)
-Test_loader = DataLoader(Testing_data, batch_size=batch)
-
-model = NN1()
-loss_fcn = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-train_losses = []
-test_losses = []
-
-# Training Loop
-for epoch in range(num_epochs):
-    model.train()
-
-    train_loss = 0.0
-    for batch, (X,y) in enumerate(Train_loader):
-        y = y.float().unsqueeze(1)
-        optimizer.zero_grad()
-        output = model(X)
-        loss = loss_fcn(output, y)
-        loss.backward()
-        optimizer.step()
-
-        train_loss += loss.item()
-    train_losses.append(train_loss)
-
-    # Testing
-    model.eval()
-    
-    test_loss = 0.0
-    
-    with torch.no_grad():
-        for batch, (X,y) in enumerate(Test_loader):
-            y = y.float().unsqueeze(1)
-            test_output = model(X)
-            loss = loss_fcn(test_output, y)
-
-            test_loss += loss.item()
-        test_losses.append(test_loss)
-
-plt.plot(range(num_epochs),train_losses, label = "Train Loss")
-plt.plot(range(num_epochs),test_losses, label = "Test Loss")
-plt.legend()
-plt.show()
-
-print(train_losses)
-print(test_losses)
-
-
- # test a few different set ups - arch, lr, batch_size, num_epochs
- # pick final setup based on perf
- # retrain final setup on whole training dataset
- # evaluate on test set       
 
 
 
