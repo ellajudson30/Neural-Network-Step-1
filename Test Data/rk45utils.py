a99796edfabbb80f3d0c85fb5eccd8be026c6086
Ella Judson <ellajudson30@gmail.com>
2026-07-09 11:23:49 -0700

    Commented functions

--- Test Data/rk45utils.py
+++ Test Data/rk45utils.py
@@ -3,6 +3,10 @@ from scipy.integrate import RK45
 
 # Function to run RK45
 def run_RK45(fcn, t0, y0, tf, tol):
+    """y' = fcn(t,y)
+    time span: [t0,tf]
+    y0 - initial value
+    tol - tolerance (both atol and rtol for RK45 solver)"""
 
     solver = RK45(fcn,t0, y0, tf, rtol=tol, atol=tol)
     
@@ -31,16 +35,31 @@ def run_RK45(fcn, t0, y0, tf, tol):
 
         errors.append(float(err))
 
-    return times, solution, time_steps, errors, stage_history
+    return (
+        np.array(times), 
+        np.array(solution), 
+        np.array(time_steps), 
+        np.array(errors), 
+        stage_history)
 
 # Approximate derviatives
 def approx_first_deriv2(sol, times):
+    """Given a sketelon of a solution, use a finite difference stencil on a 
+    non-uniform grid to approximate the first derivative
+    sol - skeleton of solution
+    times - points where skeleton is (can use to get see timesteps taken) """
+    
     y_prime = np.zeros(len(times) -2 )
 
     y_prime = (np.array(sol[2:]) - np.array(sol[:-2]))/(np.array(times[2:])
                                                      - np.array(times[:-2]))
     return y_prime
 def approx_second_deriv2(sol, times):
+    """Given a sketelon of a solution, use a finite difference stencil on a 
+    non-uniform grid to approximate the second derivative 
+    sol - skeleton of solution
+    times - points where skeleton is (can use to get see timesteps taken) """
+
     y_2prime = np.zeros(len(times) -2 )
 
     times = np.array(times)
